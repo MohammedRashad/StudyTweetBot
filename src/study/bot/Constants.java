5e27227e362379a2f651d7b09b24d403e6bda61f
Mohamed Rashad <thegreatrashad@yahoo.com>
2016-01-22 21:45:44 +0200

    V1.1.3

--- src/study/bot/Constants.java
+++ src/study/bot/Constants.java
@@ -16,5 +16,42 @@ public class Constants {
      static final String TWITTER_ACCESS_TOKEN = "";
      static final String TWITTER_ACCESS_TOKEN_SECRET = "";
      
+          static final String SEARCH_QUERY_1 = " نفسي اذاكر";
+     static final String SEARCH_QUERY_2 = "عايز اذاكر ";
+     static final String SEARCH_QUERY_3 = "اذاكر ";
+     static final String SEARCH_QUERY_4 = " مبذاكرش";
+     static final String SEARCH_QUERY_5 = " نذاكر";
+     static final String SEARCH_QUERY_6 = " هسقط";
+     static final String SEARCH_QUERY_7 = " فاينالز";
+     static final String SEARCH_QUERY_8 = " هشيل";
+          
+     static final String REPLY_QUERY_1 =  "ذاكر(ي)";
+     static final String REPLY_QUERY_2 =  "مبتذاكرش ليه؟";
+     static final String REPLY_QUERY_3 =  "طب خش ذاكر";
+     static final String REPLY_QUERY_4 =  "انا بقول تخشوا تذاكروا";
+     static final String REPLY_QUERY_5 =  "نخش نذاكر بقي؟";
+     static final String REPLY_QUERY_6 =  "الفاينال لا يرحم";
+     static final String REPLY_QUERY_7 =  "خش ذاكر";
      
+     
+     static final String TWEET_QUERY_1 =  "خش ذاكر";
+     static final String TWEET_QUERY_2 =  "مبتذاكرش ليه؟";
+     static final String TWEET_QUERY_3 =  "خشوا ذاكروا يا شباب";
+     static final String TWEET_QUERY_4 =  "يلا نذاكر";
+     static final String TWEET_QUERY_5 =  "نخش نذاكر بقي؟";
+     static final String TWEET_QUERY_6 =  "حتي الروبوتس بتذاكر يا جماعه";
+     static final String TWEET_QUERY_7 =  "انا بقول تخشوا تذاكروا";
+     static final String TWEET_QUERY_8 =  "خشوا ذاكروا";
+     static final String TWEET_QUERY_9 =  "مبتذاكروش ليه؟";
+     static final String TWEET_QUERY_10 =  "خشوا ذاكروا يا علوق";
+     static final String TWEET_QUERY_11 =  " هوبا يلا نذاكر";
+     static final String TWEET_QUERY_12 =  "نخش نذاكر بقي ولا ايه؟";
+     static final String TWEET_QUERY_13 =  "الفاينال لا يرحم";
+     static final String TWEET_QUERY_14 =  "انا بقول تخشوا تذاكروا احسن ";
+
+     static final String DONE_1 = "Done Step 1 of 2 ...";
+     static final String DONE_2 = "Done Step 2 of 2 ...";
+     static final String SLEEP = "Sleeping...";
+     static final String REPEAT = "Repeat!!!";
+
 }
